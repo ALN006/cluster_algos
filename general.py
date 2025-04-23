940094cfd1ede2878a4647cd7fd20160c6aae853
Aditya LN <adityaln80@gmail.com>
2025-04-23 15:03:44 +0530

    new class cluster added to general.py

--- general.py
+++ general.py
@@ -1,14 +1,15 @@
 import pylab
 
 class point (object):
-    """assumes self is a n-dimensional position vector for a point, models self as a array of attributes
-    provides methods to calculate the manhattan and euclidean distance between two points"""
+    """assumes self is a n-dimensional position vector for a point, models self as a array of attributes"""
 
-    def __init__(self, attribute_array):
-        self.attributes = attribute_array
+    def __init__(self, attribute_list):
+        self.attributes = pylab.array(attribute_list)
     def __str__(self):
         return str(self.attributes)
     def __eq__(self, other):
+        if len(self.attributes) != len(other.attributes):
+            return False
         for i in range(len(self.attributes)):
             if self.attributes[i] != other.attributes[i]:
                 return False
@@ -28,15 +29,70 @@ class point (object):
         self.attributes = self.attributes * scalar
     def length(self):
         return sum(self.attributes**2)**0.5
-    
-    def get_attributes(self):
-        return self.attributes
-    def get_attribute(self, index):
-        return self.attributes[index]
-    def get_dimensionality(self):
+    def dimensionality(self):
         return len(self.attributes)
+    
+    def attributes(self):
+        return self.attributes
+    def attribute(self, index):
+        return self.attributes[index]
     
     def set_attributes(self, attribute_array):
         self.attributes = attribute_array
     def set_attribute(self, index, value):
         self.attributes[index] = value
+
+class cluster(object):
+    """assumes self is a cluster of points, models self as a list of point objects"""
+    def __init__(self, points):
+        self.points = points
+    def __str__(self):
+        return str(self.points)
+    def __eq__(self, other):
+        if len(self.points) != len(other.points):
+            return False
+        for i in range(len(self.points)):
+            if self.points[i] != other.points[i]:
+                return False
+        return True
+    
+    def centroid(self):
+        """returns the centroid of self as a point object"""
+        centroid = point([0]*self.points[0].dimensionality())
+        for p in self.points:
+            centroid = centroid.plus(p)
+        centroid.scale(1/len(self.points))
+        return centroid
+    def single_linkage(self, other, distance_function):
+        """returns shortest distance between self and other"""
+        min_distance = self.points[0].distance_function(other.points[0])
+        for p1 in self.points:
+            for p2 in other.points:
+                distance = p1.distance_function(p2)
+                if distance < min_distance:
+                    min_distance = distance
+        return min_distance
+    def complete_linkage(self, other, distance_function):
+        """returns longest distance between self and other"""
+        max_distance = self.points[0].distance_function(other.points[0])
+        for p1 in self.points:
+            for p2 in other.points:
+                distance = p1.distance_function(p2)
+                if distance > max_distance:
+                    max_distance = distance
+        return max_distance
+    def centroid_linkage(self, other, distance_function):
+        """returns distance between centroids of self and other"""
+        return self.centroid().distance_function(other.centroid())
+    
+    def add(self, point):
+        self.points.append(point)
+    def remove(self, point):
+        self.points.remove(point)
+    def merge(self, other):
+        self.points.extend(other.points)
+    def clear(self):
+        self.points = []
+    
+    def points(self):
+        return self.points
