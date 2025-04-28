c0bdf9d705232e7c87f2f04b43dc219d4b8c785b
Aditya LN <adityaln80@gmail.com>
2025-04-28 13:22:10 +0530

    Clean up code

--- general.py
+++ general.py
@@ -1,204 +1,184 @@
 import pylab
-import numpy as np
 import pandas as pd
-import random
+import numpy as np
 class point (object):
     """assumes self is a n-dimensional position vector for a point, models self as a array of attributes"""
 
-    def __init__(self, attributes = []):
-        self.series = pd.Series(attributes)
-    def __str__(self):
-        return str(list(self.series))
+    def __init__(self, attributes):
+        self.attributes = np.array(attributes)
     def __repr__(self):
-        return str(list(self.series))
+        return str(self.attributes)
     def __eq__(self, other):
-        return (self.series == other.series).all()
+        return len(self.attributes) == len(other.attributes) and (abs(self.attributes - other.attributes) > 0.0001).all()
+    def __hash__(self):
+        return hash(tuple(self.attributes))
     
     def __add__(self, other):
         if isinstance(other, point):
-            return point(self.series + other.series)
+            return point(self.attributes + other.attributes)
         else:
-            return point(self.series + other)
+            return point(self.attributes + other)
     def __sub__(self, other):
         return self + other * -1
     def __mul__(self, other):
         if isinstance(other, point):
-            return point(self.series * other.series)
+            return point(self.attributes * other.attributes)
         else:
-            return point(self.series * other)
+            return point(self.attributes * other)
+    def __truediv__(self, other):  
+        if isinstance(other, point):
+            return point(self.attributes / other.attributes)
+        else:
+            return point(self.attributes / other)
     def __pow__(self, other):
         if isinstance(other, point):
-            return point(self.series ** other.series)
+            return point(self.attributes ** other.attributes)
         else:
-            return point(self.series ** other)
+            return point(self.attributes ** other)
     
     def __len__(self):
-        return len(self.series)
+        return len(self.attributes)
     def __getitem__(self, index):
-        return self.series[index]
+        return self.attributes[index]
     def __setitem__(self, index, value):
-        self.series[index] = value
+        self.attributes[index] = value
     def __iter__(self): 
-        return iter(self.series)
+        return iter(self.attributes)
     def __contains__(self, item): 
-        return item in self.series.values
+        return item in self.attributes
     
     def length(self):
-        return self.euclidean_distance(point([0]*len(self.series)))
+        return self.euclidean_distance(point([0]*len(self.attributes)))
     def manhattan_distance(self, other):
-        return sum(abs(self.series - other.series))
+        return sum(abs(self.attributes - other.attributes))
     def euclidean_distance(self, other):
-        return sum((self.series - other.series)**2)**0.5
+        return sum((self.attributes - other.attributes)**2)**0.5
     def dot(self, other):
-        return sum(self.series * other.series)
+        return sum(self.attributes * other.attributes)
+    def copy(self):
+        return point(self.attributes)
     
     def get_attributes(self):
-        return list(self.series)
+        return list(self.attributes)
     def set_attributes(self, attributes):
-        self.series = pd.Series(attributes)
+        self.attributes = np.array(attributes)
 
 class cluster(object): 
     """assumes self is a cluster of points, models self as a list of point objects"""
-    def __init__(self, points = []):
-        self.points = points
-        self.df = pd.DataFrame([p.get_attributes() for p in points])
-    def __str__(self):
-        return str(self.df)
-    def __eq__(self, other):
-        return (self.df == other.df).all().all()
+
+    def __init__(self, data):
+        points = data.values.tolist() if isinstance(data, pd.DataFrame) else data
+        self.points= []
+        self.d = {}
+        for p in points:
+            self.points.append(p)
+            if p in self.d:
+                self.d[p] += 1
+            else:
+                self.d[p] = 1
     def __repr__(self):
-        return str(self.df)
+        return str(self.points)
+    def __eq__(self, other):
+        return self.d == other.d
+    
     def __add__(self, other):
         return cluster(self.points + other.points)
     def __sub__(self, other):
-        """returns a new cluster with all points in self that are not in other"""
-        return cluster([p for p in self.points if p not in other.points])
+        points = []
+        for i in self.d:
+            count_in_other = other.d.get(i, 0)
+            for j in range(self.d[i] - count_in_other if self.d[i] > count_in_other else 0):
+                points.append(i)
+                
+        return cluster(points)
+    def __mul__(self, other):
+        points = []
+        if isinstance(other, cluster):
+            for i in range(len(self.points)):
+                points.append(self.points[i] * other.points[i])
+            return cluster(points)
+        else:
+            for i in range(len(self.points)):
+                points.append(self.points[i] * other)
+            return cluster(points)
+    def __pow__(self, integer):
+        points = []
+        for i in range(len(self.points)):
+            points.append(self.points[i] ** integer)
+        return cluster(points)
+    
+    def __len__(self):
+        return len(self.points)
+    def __getitem__(self, index):
+        return self.points[index]
+    def __setitem__(self, index, value):
+        self.points[index] = value
+    def __iter__(self): 
+        return iter(self.points)
+    def __contains__(self, item): 
+        return item in self.points
     
     def centroid(self):
-        """returns the centroid of self as a point object"""
-        return point([sum(self.df[i])/len(self.df) for i in range(len(self.df))])
-    def intensive_linkage(self, other,link = "single", distance_function = point.euclidean_distance):
-        """returns linkage vector between self and other"""
-        link_f = {"single": (lambda x,y: x < y), "complete": (lambda x,y: x > y)}
-        d = distance_function(self.df.loc[0],other.df.loc[0])
-        ans = self.df.loc[0] - other.df.loc[0]
-        for i in range(len(self.df)):
-            for j in range(len(other.df)):
-                distance = distance_function(self.df.loc[i],other.df.loc[j])
-                if link_f[link](distance, d):
-                    d = distance
-                    ans = self.df.loc[i] - other.df.loc[j]
-        return point(ans)
-    def centroid_linkage(self, other):
-        return (self.centroid() - other.centroid())
-    def scaled(self, scalar):
-        return cluster([self.df.loc[i]*scalar for i in range(len(self))])#this should work
+        return point(sum(self.points) * (1/len(self.points)))
+    def DistanceMatrix(self,other, dist_func = point.euclidean_distance):
+        matrix = []
+        for i in self.points:
+            row = []
+            for j in other.points:
+                row.append(dist_func(i,j))
+            matrix.append(row)
+        return matrix
+    def intensive_linkage(self, other, type = "single", dist_func = point.euclidean_distance):
+        matrix = self.DistanceMatrix(other, dist_func)
+        if type == "single":    
+            return min([min(i) for i in matrix])
+        elif type == "complete":        
+            return max([max(i) for i in matrix])
+        elif type == "average":                 
+            return sum([sum(i) for i in matrix]) / (len(matrix) * len(matrix[0]))
+    def centroid_linkage(self, other, dist_func = point.euclidean_distance):
+        return dist_func(self.centroid(), other.centroid())
+    def normalize(self):
+        max_val = self.points[0].copy()
+        min_val = self.points[0].copy()
+        for i in self.points:
+            for j in range(len(self.points[0])):
+                if i[j] > max_val[j]:
+                    max_val[j] = i[j]
+                if i[j] < min_val[j]:
+                    min_val[j] = i[j]
+        return cluster([(i - min_val)/(max_val - min_val) for i in self.points])
     
-    def get_df(self):
-        return self.df
+    def get_points(self):
+        return self.points
+    def get_d(self):
+        return self.d
     
-    def append(self, point):
-        self.points.append(point)
-    def remove(self, point):
-        self.points.remove(point)
-    def clear(self):
-        self.points = []
-    def normalized(self):
-        points = self.points.copy()
-        mins = points[0].get_attributes()
-        maxs = points[0].get_attributes()
-        n = len(points[0])
+    def reset(self,points: list[point]):
+        self.points= []
+        self.d = {}
         for p in points:
-            for i in range(n):
-                if p.attribute(i) < mins[i]:
-                    mins[i] = p.attribute(i)
-                if p.attribute(i) > maxs[i]:
-                    maxs[i] = p.attribute(i)
-        for p in points:
-            p.set_attributes((p.get_attributes() - mins) / (maxs - mins))
-        return cluster(points)
+            self.points.append(p)
+            if p in self.d:
+                self.d[p] += 1
+            else:
+                self.d[p] = 1
 
-class test_point(object):
+import unittest     
+import random  
+class test_point(unittest.TestCase):
+
+    def setUp(self):
+        self.trials = 100
+        self.dimensions = 3
+        self.points = [[ random.random() for _ in range(self.dimensions)] for __ in range(self.trials)]
     
-    def __init__(self, trials, dimensions):
-        self.dimensions = dimensions
-        self.trials = trials
+    def test_string(self):
+        for i in self.points:
+            self.assertEqual(str(np.array(i)), str(point(i)))
+    
+if __name__ == '__main__':
+    unittest.main()
 
-    def add(self):
-        '''tests __add__,__sub__,__init__,__eq__,__repr,__str__'''
-        for i in range(self.trials):
-            L1 = [random.random() for j in range(self.dimensions)]
-            L2 = [random.random() for j in range(self.dimensions)]
-            p1 = point(L1)
-            p2 = point(L2)
-            if point((p1 + p2).get_attributes()) != point(pylab.array(L1) + pylab.array(L2)):
-                print(f"add failed\n p1 = {p1}\n p2 = {p2}\n p1 + p2 = {p1 + p2}")
-                return False
-        return True
-    def mul(self):
-        for i in range(self.trials):
-            L1 = [random.random() for j in range(self.dimensions)]
-            L2 = [random.random() for j in range(self.dimensions)]
-            p1 = point(L1)
-            p2 = point(L2)
-            if (p1 * p2) != point(pylab.array(L1) * pylab.array(L2)):
-                print(f"mul failed\n p1 = {p1}\n p2 = {p2}\n p1 * p2 = {p1 * p2}")
-                return False
-        return True
-    def len(self):
-        for i in range(self.trials):
-            L = [random.random() for j in range(self.dimensions)]
-            p = point(L)
-            if len(p) != len(L):
-                print(f"len failed\n p = {p}\n len(p) = {len(p)}\n len(L) = {len(L)}")
-                return False
-        return True
-    def manhattan_distance(self):
-        for i in range(self.trials):
-            L1 = [random.random() for j in range(self.dimensions)]
-            L2 = [random.random() for j in range(self.dimensions)]
-            p1 = point(L1)
-            p2 = point(L2)
-            if p1.manhattan_distance(p2) != sum(abs(pylab.array(L1) - pylab.array(L2))):
-                print(f"manhattan_distance failed\n p1 = {p1}\n p2 = {p2}\n p1.manhattan_distance(p2) = {p1.manhattan_distance(p2)}")
-                return False
-        return True
-    def euclidean_distance(self):
-        for i in range(self.trials):
-            L1 = [random.random() for j in range(self.dimensions)]
-            L2 = [random.random() for j in range(self.dimensions)]
-            p1 = point(L1)
-            p2 = point(L2)
-            if p1.euclidean_distance(p2) != sum((p1 - p2)**2)**0.5:
-                print(f"euclidean_distance failed\n p1 = {p1}\n p2 = {p2}\n p1.euclidean_distance(p2) = {p1.euclidean_distance(p2)}")
-                return False
-        return True
-    def assignment(self):
-        for i in range(self.trials):
-            L = [random.random() for j in range(self.dimensions)]
-            p = point()
-            p.set_attributes(L)
-            x = p[0]
-            p[0] = x + 1
-            L[0] = x + 1
-            if p[0] == x or p[0] != x + 1 or p.get_attributes() != L:
-                print(f"item_assignment failed\n p = {p}\n intial val of p[0] -> x = {x}\n final value is not incremented by 1 -> p[0] = {p[0]}")
-                return False
-        return True
-    def iteration(self): 
-        for i in range(self.trials):
-            L = [random.random() for j in range(self.dimensions)]
-            p = point(L)
-            for k in p:
-                if k not in p:
-                    print(f"iteration failed\n p = {p}\n k = {k}\n k not in p = {k not in p}")
-                    return False
-        return True
- 
-    def TestAll(self):
-        result = True
-        tests = [self.add, self.mul, self.len, self.manhattan_distance, self.euclidean_distance, self.assignment, self.iteration]
-        for t in tests:
-            if not t():
-                result = False
-        return result
+    
+    
